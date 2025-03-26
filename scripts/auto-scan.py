6ce601315d1b73ea30f34fdae5f6aea20b4fda5c
tfs <tfs@kali>
2025-03-26 03:15:35 -0500

    in progress auto scan with python

--- scripts/auto-scan.py
+++ scripts/auto-scan.py
@@ -1,2 +1,13 @@
 import sys
 import os
+import subprocess
+from module import dirsearch
+
+
+output_dir = "auto-scan-result"
+os.makedirs(output_dir, exist_ok=True)
+
+def sanitize_name(target)
+    return re.sub(r'[^a-zA-Z0-9]', '_', target)
+
+def run_scan(target, output_file)
