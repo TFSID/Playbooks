526d9f9734350118de65321fe42d9bc864aabc25
rhaihanadzani <mrhaihan17@gmail.com>
2025-04-24 11:21:07 +0700

    jelek

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,14 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   // Your configuration options here
   vite: {
     server: {
-      host: '0.0.0.0',
+      host: "0.0.0.0",
       port: 4321,
-    }
-  }
+    },
+    plugins: [tailwindcss()],
+  },
 
+  integrations: [react()],
 });
-
